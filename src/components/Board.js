1c6dd446a7429998fc4c641673898f0a2c9973a8
Isobel Betteridge <isobel.betteridge@arnoldclark.com>
2020-10-31 00:46:33 +0000

    added white containing box

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import Square from './Square'
 
-
 function Board({ x, y }) {
 
-    return (new Array(x)
-        .fill(null)
-        .map((x, i) =>
-            <div key={i}>
-                {new Array(y)
-                    .fill(null)
-                    .map((y, j) =>
-                        <Square key={j}
-                            left={false}
-                            right={false}
-                            up={false}
-                            down={true}
-                             />)
-                }
-            </div>
-        )
+    return (
+        <div className="boardBox">{new Array(x)
+            .fill(null)
+            .map((x, i) =>
+                <div key={i}>
+                    {new Array(y)
+                        .fill(null)
+                        .map((y, j) =>
+                            <Square key={j}
+                                left={false}
+                                right={false}
+                                up={false}
+                                down={true}
+                            />)
+                    }
+                </div>
+            )
+        }</div>
     )
+
+
 }
 
 export default Board;
